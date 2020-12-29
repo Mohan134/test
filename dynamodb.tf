fd29924dade58594b0711e4623d77fd5778754f6
ravinder-1997 <56959562+ravinder-1997@users.noreply.github.com>
2020-12-29 09:53:29 +0530

    Add files via upload

---
+++ dynamodb.tf
@@ -0,0 +1,26 @@
+resource "aws_dynamodb_table" "ddbtable" {
+  name             = "myDB"
+  hash_key         = "id"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+# store the content in dynamo db table 
+resource "aws_dynamodb_table_item" "example" {
+  table_name = "${aws_dynamodb_table.myDB.name}"
+  hash_key   = "${aws_dynamodb_table.id.hash_key}"
+
+  item = <<ITEM
+{
+  "exampleHashKey": {"S": "something"},
+  "one": {"N": "11111"},
+  "two": {"N": "22222"},
+  "three": {"N": "33333"},
+  "four": {"N": "44444"}
+}
+ITEM
+}
